7193033f4ce78923803a4b060ecea19f80a5158a
Nicolas Mauricio Martins Coiado <48792614+NicolasCoiado03@users.noreply.github.com>
2021-08-31 17:23:31 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# app-agendamentos
+ App feito utilizando Cordova e Firebase com o intúito de treinar a conexão entre ambos.
---
+++ www/js/firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyC_-RzcVzgJFbbremxm5pMZdbuSaoDg5vQ",
+    authDomain: "app-agendamentos-5dcec.firebaseapp.com",
+    projectId: "app-agendamentos-5dcec",
+    storageBucket: "app-agendamentos-5dcec.appspot.com",
+    messagingSenderId: "590490249572",
+    appId: "1:590490249572:web:a6fb6a03f9a28b8f09f271",
+    measurementId: "G-MGP63SR394"
+ };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
